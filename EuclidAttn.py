ad450f71412398e20cd9c1f58221da249d258f9e
toshiouchi <121741811+toshiouchi@users.noreply.github.com>
2024-03-07 04:14:23 +0900

    Update EuclidAttn.py

--- EuclidAttn.py
+++ EuclidAttn.py
@@ -1,8 +1,33 @@
 import torch
 import torch.nn as nn
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, dim_hidden: int, n_head: int, qkv_bias: bool=False):
+        super().__init__()
+        self.n_head = n_head
+
+        self.proj_q = nn.Linear( dim_hidden, dim_hidden, bias=qkv_bias)
+        self.proj_k = nn.Linear( dim_hidden, dim_hidden, bias=qkv_bias)
+        self.proj_v = nn.Linear( dim_hidden, dim_hidden, bias=qkv_bias)
+        self.proj_out = nn.Linear(dim_hidden, dim_hidden)
+
+        self.qkv_attention = ScaledEuclidDistanceAttention( dim_hidden, n_head )
+        
+    def forward( self,query,key,value, attn_mask = None ):
+
+        q = self.proj_q( query )
+        k = self.proj_k( key )
+        v = self.proj_v( value )
+        
+        output = self.qkv_attention(q, k, v, attn_mask)
+
+        output = self.proj_out( output )
+        
+        return output 
+
+    
 class ScaledEuclidDistanceAttention(nn.Module):
-    def __init__(self, dim_hidden: int, num_heads: int, qkv_bias: bool=False):
+    def __init__(self, dim_hidden: int, num_heads: int, dropout: float=0.1):
         super().__init__()
 
         assert dim_hidden % num_heads == 0
@@ -13,18 +38,10 @@ class ScaledEuclidDistanceAttention(nn.Module):
 
         # Scale Value of Softmax
         self.scale = dim_head ** -0.5
+        
+        self.dropout = nn.Dropout(dropout)
 
-        self.proj_q = nn.Linear( dim_hidden, dim_hidden, bias=qkv_bias)
-        self.proj_k = nn.Linear( dim_hidden, dim_hidden, bias=qkv_bias)
-        self.proj_v = nn.Linear( dim_hidden, dim_hidden, bias=qkv_bias)
-
-        self.proj_out = nn.Linear(dim_hidden, dim_hidden)
-
-    def forward(self, q, k, v, mask = None ):
-
-        q = self.proj_q( q )
-        k = self.proj_k( k )
-        v = self.proj_v( v )
+    def forward(self, q, k, v, mask=None ):
 
         q = q.view( q.size(0), q.size(1), self.num_heads, -1 ).permute( 0,2,1,3 )
         k = k.view( k.size(0), k.size(1), self.num_heads, -1 ).permute( 0,2,1,3 )
@@ -37,14 +54,14 @@ class ScaledEuclidDistanceAttention(nn.Module):
             attn = attn + torch.unsqueeze( torch.unsqueeze( mask, dim = 1 ), dim = 3 ).to(torch.float16) * -1e9
         attn = ( attn ).softmax(dim=-1)  
 
+        attn = self.dropout( attn )
+        
         x = attn.matmul(v)
 
         x = x.permute(0, 2, 1, 3).flatten(2)
-        x = self.proj_out(x)
 
         return x
-        
-
+    
 if __name__ == "__main__":
 
     num_batch = 8
@@ -53,11 +70,17 @@ if __name__ == "__main__":
     dim_hidden = 512
     num_heads = 8
 
-    func2 = ScaledEuclidDistanceAttention( dim_hidden, num_heads )
+    func2 = MultiHeadAttention( dim_hidden, num_heads )
 
     q = torch.randn( ( num_batch, q_seq, dim_hidden))
     k = torch.randn( ( num_batch, k_seq, dim_hidden))
     v = k
+    mask = torch.randint(low=0, high=2, size=(num_batch,q_seq)).to( torch.bool )
+
+    x = func2( q,k,v,mask)
+
+    print( x.size() )
+
     mask = torch.randint(low=0, high=2, size=(num_batch,q_seq)).to( torch.bool )
 
     x = func2( q,k,v,mask)
